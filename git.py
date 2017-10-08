85b8a5689ece4ba9c85415b1f71604ecc3088c42
gangwals <s.gangwal@yahoo.in>
2017-10-08 11:27:51 +0530

    "update"

--- git.py
+++ git.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+
 from subprocess import call
-call(["ls", "-l"])
+
 
 call(["git", "add", "-A"])
-#os.system("git commit -m "update"")
-#os.system("git push")
+call(["git", "commit", "-m", "\"update\""])
+call(["git", "push"])
